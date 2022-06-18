e01ff0ce9457c1634c2ebaf71b77b517cb33414f
kamrul <kamrul@asabd.org>
2022-06-18 16:30:46 +0600

    Model added

---
+++ Model/Amounts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Amounts : BaseEntity
+    {
+        public int MemberId { get; set; }
+        public int Amount { get; set; }
+        public DateTime AmountDate { get; set; }
+        public int DipositeId { get; set; }
+    }
+}
---
+++ Model/Deposites.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Deposites : BaseEntity
+    {
+        public int MemberId { get; set; }
+        public int AmountId { get; set; }
+        public DateTime DepositeDate { get; set; }
+    }
+}
