ac9dcf55d0387cb946414c76e05340a7918c77ac
kamrul <kamrul@asabd.org>
2022-06-20 16:37:49 +0600

    Error and token IService added

---
+++ Core/Service/IErrorService.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IErrorService
+    {
+        void SaveError(Error item);
+        List<Error> GetErrors(int errorId);
+    }
+}
---
+++ Core/Service/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims, DateTime expirationTime);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
