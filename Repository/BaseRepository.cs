f3169a5aa9fccaafa3e931f3d6ae0e15361f156b
kamrul <kamrul@asabd.org>
2022-06-22 16:39:16 +0600

    Base Repository Added

---
+++ Repository/BaseRepository.cs
@@ -0,0 +1,86 @@
+﻿using Core.Common;
+using Core.Repository;
+using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        private MCLDBContext _Context = null;
+        private DbSet<T> _Entities = null;
+        private IUserResolverService _User = null;
+
+        public BaseRepository(MCLDBContext context, IUserResolverService user)
+        {
+            _Context = context;
+            _User = user;
+            _Entities = _Context.Set<T>();
+        }
+
+        public IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            IQueryable<T> query = _Entities.AsNoTracking().AsQueryable<T>().Where(predicate).Where(x => x.IsActive == true);
+            return query;
+        }
+
+        public IQueryable<T> FindBy(Expression<Func<T, bool>> predicate, int skip, int take)
+        {
+            IQueryable<T> query = _Entities.AsNoTracking().AsQueryable<T>().Where(predicate).Where(x => x.IsActive == true).Skip(skip).Take(take);
+            return query;
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            IQueryable<T> query = _Entities.AsNoTracking().AsQueryable<T>().Where(x => x.IsActive == true);
+            return query;
+        }
+
+        public virtual T GetById(int id)
+        {
+            T entity = _Entities.AsNoTracking().FirstOrDefault(x => x.Id == id);
+            return entity;
+        }
+
+        public virtual T Add(T entity)
+        {
+            _Context.Entry(entity).State = EntityState.Added;
+            entity = _Entities.Add(entity).Entity;
+            _Context.SaveChanges();
+            return entity;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _Context.Entry(entity).State = EntityState.Modified;
+            entity.IsActive = false;
+            _Entities.Update(entity);
+            _Context.SaveChanges();
+        }
+
+        public virtual void HardDelete(T entity)
+        {
+            _Context.Entry(entity).State = EntityState.Deleted;
+            _Entities.Remove(entity);
+            _Context.SaveChanges();
+        }
+
+        public virtual T Update(T entity)
+        {
+            _Context.Entry(entity).State = EntityState.Modified;
+            entity = _Entities.Update(entity).Entity;
+            entity.ModificationDate = DateTime.UtcNow;
+            int userId;
+            int.TryParse(_User.GetUser(), out userId);
+            entity.ModifiedBy = (userId == 0 ? null : userId);
+            _Context.SaveChanges();
+            return entity;
+        }
+    }
+}
---
+++ Repository/MCLDBContext.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class MCLDBContext : DbContext
+    {
+        public MCLDBContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        public virtual DbSet<AuditLog> AuditLogs { get; set; }
+        public virtual DbSet<Amounts> Amounts { get; set; }
+        public virtual DbSet<Deposites> Deposites { get; set; }
+        public virtual DbSet<Members> Members { get; set; }
+        public virtual DbSet<Role> Role { get; set; }
+        public virtual DbSet<RoleUser> RoleUser { get; set; }
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<UserRole> UserRole { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasMany(u => u.Roles).WithMany(x => x.Users).UsingEntity<RoleUser>(
+                x => x.HasOne(xs => xs.Role).WithMany(),
+                x => x.HasOne(xs => xs.User).WithMany()
+                ).HasKey( x => new {x.UserId, x.RoleId});
+        }
+
+        protected virtual long GetPrimaryKeyValue<T>(T entity)
+        {
+            var test = entity;
+            var test2 = test.GetType();
+            var keyName = this.Model.FindEntityType(test2).FindPrimaryKey().Properties.Select(x => x.Name).Single();
+
+            object obj = entity.GetType().GetProperty(keyName).GetValue(entity, null);
+            var result = (obj is int) ? (int)obj : (long)obj;
+            if(result < 0)
+            {
+                return -1;
+            }
+            return result;
+        }
+
+        public int SaveChanges(string userId)
+        {
+            if(userId == null)
+            {
+                return base.SaveChanges();
+            }
+
+            try
+            {
+                ChangeTracker.DetectChanges();
+                IList<AuditLog> auditLogChanges = new List<AuditLog>();
+                var modifiedEntities = ChangeTracker.Entries().Where( p => p.State == EntityState.Modified || p.State == EntityState.Detached || p.State == EntityState.Detached).ToList();
+
+                foreach(var change in modifiedEntities)
+                {
+                    TableAttribute tableAttribute = change.Entity.GetType().GetCustomAttributes(typeof(TableAttribute), false).SingleOrDefault() as TableAttribute;
+                    string tableName = tableAttribute != null ? tableAttribute.Name : change.Entity.GetType().Name;
+                    var entityName = change.Entity.GetType().Name;
+                    var primaryKeyValue = GetPrimaryKeyValue(change.Entity);
+
+                    if (change.State == EntityState.Added)
+                    {
+                        var changeLoged = new AuditLog
+                        {
+                            ColumnName = "All",
+                            RecordID = primaryKeyValue.ToString(),
+                            EventDateUTC = DateTime.UtcNow,
+                            OriginalValue = "",
+                            NewValue = Newtonsoft.Json.JsonConvert.SerializeObject(change.Entity),
+                            UserID = userId,
+                            EventType = change.State.ToString().Substring(0, 1),
+                            TableName = tableName
+                        };
+                        AuditLogs.Add(changeLoged);
+                    }
+                    else
+                    {
+                        foreach(var prop in change.Entity.GetType().GetTypeInfo().GetRuntimeProperties())
+                        {
+                            if(prop.Name == "ModifiedBy" || prop.Name == "ModifiedDate")
+                            {
+                                continue;
+                            }
+
+                            if(!prop.GetGetMethod().IsVirtual && prop.CustomAttributes.Where(x => x.AttributeType.Name == "NotMappedAttribute").Count() == 0)
+                            {
+                                try
+                                {
+                                    var currentValue = change.Property(prop.Name).CurrentValue ?? string.Empty;
+                                    var originalValue = change.Property(prop.Name).OriginalValue ?? string.Empty;
+
+                                    if(change.Property(prop.Name).CurrentValue != null && change.Property(prop.Name).OriginalValue != null 
+                                        && change.Property(prop.Name).CurrentValue.GetType() == typeof(decimal))
+                                    {
+                                        currentValue = ((decimal)change.Property(prop.Name).CurrentValue).ToString("0.00");
+                                        originalValue = ((decimal)change.Property(prop.Name).OriginalValue).ToString("0.00");
+                                    }
+
+                                    if(originalValue.ToString() != currentValue.ToString() || change.State == EntityState.Added || change.State == EntityState.Deleted)
+                                    {
+                                        var changeLoged = new AuditLog
+                                        {
+                                            ColumnName = prop.Name,
+                                            RecordID = primaryKeyValue.ToString(),
+                                            EventDateUTC = DateTime.UtcNow,
+                                            OriginalValue = originalValue.ToString(),
+                                            NewValue = currentValue.ToString(),
+                                            UserID = userId,
+                                            EventType = change.State.ToString().Substring(0, 1),
+                                            TableName = tableName
+                                        };
+                                        auditLogChanges.Add(changeLoged);
+                                    }
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                        }
+                    }
+                }
+                if(auditLogChanges.Count > 0)
+                {
+                    AuditLogs.AddRange(auditLogChanges);
+                }
+                return base.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return 0;
+            }
+        }
+    }
+}
