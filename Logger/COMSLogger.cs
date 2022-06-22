913a4ce3388a586b35d3370a4f79436c516f6a7f
kamrul <kamrul@asabd.org>
2022-06-22 15:06:01 +0600

    Logger Added

---
+++ Logger/COMSLogger.cs
@@ -0,0 +1,94 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class COMSLogger : ICOMSLogger
+    {
+        static Serilog.ILogger _logger;
+        static string _logLevel = "Information";
+        static string _filePath = Path.Combine(Environment.CurrentDirectory, "logs", "app_log.log");
+
+        public COMSLogger() : this(_filePath, _logLevel)
+        {
+
+        }
+
+        public COMSLogger(string filpPath) : this(filpPath, _logLevel)
+        {
+
+        }
+
+        public COMSLogger(string filpPath, string logLevel)
+        {
+            if(_logger == null)
+            {
+                _logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.Console().WriteTo
+                    .File(
+                    path: filpPath,
+                    rollingInterval: RollingInterval.Day,
+                    fileSizeLimitBytes: 1048760,
+                    rollOnFileSizeLimit: true,
+                    retainedFileCountLimit: 3
+                    ).CreateLogger();
+            }
+        }
+
+        public void Information(string logText)
+        {
+            _logger.Information(logText);
+        }
+
+        public void Warning(string logText)
+        {
+            _logger.Warning(logText);
+        }
+        public void Error(string logText)
+        {
+            _logger.Error(logText);
+        }
+
+        public void Debug(string logText)
+        {
+            _logger.Debug(logText);
+        }
+
+        public void Fatal(string logText)
+        {
+            _logger.Fatal(logText);            
+        }
+    }
+
+    public class NullLogger : ICOMSLogger
+    {
+        public void Debug(string logText)
+        {
+            
+        }
+
+        public void Error(string logText)
+        {
+            
+        }
+
+        public void Fatal(string logText)
+        {
+            
+        }
+
+        public void Information(string logText)
+        {
+            
+        }
+
+        public void Warning(string logText)
+        {
+            
+        }
+    }
+}
---
+++ Logger/ICOMSLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public interface ICOMSLogger
+    {
+        void Information(string logText);
+        void Warning(string logText);
+        void Error(string logText);
+        void Debug(string logText);
+        void Fatal(string logText);
+    }
+}
