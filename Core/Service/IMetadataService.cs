ad8616b47027081790546303c27befbc485ed647
kamrul <kamrul@asabd.org>
2022-06-22 12:44:32 +0600

    MetaData Added

---
+++ Core/Service/IMetadataService.cs
@@ -0,0 +1,20 @@
+﻿using Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IMetadataService
+    {
+        List<AmountResponse> GetAmounts();
+        AmountResponse GetAmount(int id);
+        List<DepositResponse> GetDeposits();
+        DepositResponse GetDeposit(int id);
+        List<MemberResponse> GetMembers();
+        MemberResponse GetMember(int id);
+
+    }
+}
