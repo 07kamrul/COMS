c0f88e2f3200982559f000481c43e3a531e45eae
kamrul <kamrul@asabd.org>
2022-07-02 11:57:01 +0600

    Member Repository Search Added

--- Repository/MemberRepository.cs
+++ Repository/MemberRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Common;
 using Core.Repository;
 using Core.RequestModels;
+using Microsoft.EntityFrameworkCore;
 using Model;
 using System;
 using System.Collections.Generic;
@@ -20,7 +21,32 @@ namespace Repository
 
         public Page<Members> Search(MemberSearchRequestModel searchModel, int skip, int take)
         {
-            
+            _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            IQueryable<Members> query = _context.Set<Members>();
+
+            query = query.Where(x => (searchModel.MemberId == 0 || (x.Email.Length > 0 && x.Id == searchModel.MemberId))
+                && (string.IsNullOrEmpty(searchModel.MemberName) || x.Name.Contains(searchModel.MemberName))
+                && (searchModel.JoiningDate ==  null || x.JoiningDate == searchModel.JoiningDate)
+                && (searchModel.DateOfBirth == null || x.DateOfBirth == searchModel.DateOfBirth)
+                && (searchModel.NID == 0 || x.NID == searchModel.NID)
+                && (string.IsNullOrEmpty(searchModel.Phone) || x.Phone.Contains(searchModel.Phone))
+                && x.IsActive
+                && x.IsVerified
+            );
+
+            return new Page<Members>
+            {
+                Data = query.OrderBy(a => a.Id).Skip(skip).Take(take)
+                    .Include(x => x.Attachments).ThenInclude(x => x.AttachmentType)
+                    .Include(x => x.Name)
+                    .Include(x => x.NID)
+                    .Include(x => x.Phone)
+                    .Include(x => x.Email)
+                    .Include(x => x.Gender)
+                    .Include(x => x.MaritalStatus)
+                    .Include(x => x.IsActive).ToList(),
+                Total = query.Count()
+            };
         }
     }
 }
