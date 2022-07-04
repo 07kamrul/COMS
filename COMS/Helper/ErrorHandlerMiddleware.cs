604fa5339f98dde10fc2a9c76ef2da83cc1307e4
kamrul <kamrul@asabd.org>
2022-07-04 13:42:16 +0600

    Helper Added

---
+++ COMS/Helper/ErrorHandlerMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace COMS.Helper
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ErrorHandlerMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch(Exception ex)
+            {
+                _logger.Error($"Something went wrong: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return httpContext.Response.WriteAsync(new ErrorDetails
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = ex.Message,
+            }.ToString());
+        }
+    }
+
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ COMS/Helper/ExportHelper.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+
+namespace COMS.Helper
+{
+    public class ExportHelper<T>
+    {
+        private readonly HttpResponse _response;
+
+        public ExportHelper(HttpResponse response)
+        {
+            _response = response;
+        }
+
+/*        public void ExportToPdf(List<T> exportData, string header)
+        {
+            byte[] bytes = exportData.ToPdf(
+                scheme =>
+                {
+                    scheme.HeaderFontBold = true;
+                    scheme.HeaderFontSize = 25;
+                    scheme.Header = header;
+                    scheme.HeaderHeight = 15;
+                    scheme.HeaderAlignment = ArrayToPdfAlignments.Center;
+                    scheme.PageMarginTop = 10;
+                });
+            _response.Clear();
+            _response.Headers.Add("content-diposition", "attachment;filename=Export.pdf");
+            _response.ContentType = "application/pdf";
+            _response.Body.WriteAsync(bytes, 0, bytes.Length);
+        }*/
+
+        public void ExportToExcel(List<T> exportData, string header)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using(MemoryStream stream = new MemoryStream())
+            using (var excelFile = new ExcelPackage(stream))
+            {
+                var worksheet = excelFile.Workbook.Worksheets.Add("Sheet1");
+                worksheet.Cells["A2"].LoadFromCollection(Collection: exportData, PrintHeaders: true);
+                worksheet.Cells[1, 1, 1, worksheet.Dimension.Columns].Merge = true;
+                worksheet.Cells[1, 1, 1, worksheet.Dimension.Columns].Style.Font.Bold = true;
+                worksheet.Cells[1, 1, 1, worksheet.Dimension.Columns].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                worksheet.Cells[1, 1, 1, worksheet.Dimension.Columns].Style.Font.Size = 25;
+                worksheet.Cells["A1"].Value = header;
+
+                using(var range = worksheet.Cells[2,1,2, worksheet.Dimension.Columns])
+                {
+                    range.Style.Font.Bold = true;
+                    range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    range.Style.Fill.BackgroundColor.SetColor(Color.Black);
+                    range.Style.Font.Color.SetColor(Color.White);
+                }
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+                worksheet.Cells[worksheet.Dimension.Address].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[worksheet.Dimension.Address].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[worksheet.Dimension.Address].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[worksheet.Dimension.Address].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+
+                _response.Clear();
+                _response.Headers.Add("content-disposition", "attachment; filename=Export.xlsx");
+                _response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                var bytes = excelFile.GetAsByteArray();
+                _response.Body.WriteAsync(bytes, 0, bytes.Length);
+
+            }
+        }
+    }
+}
