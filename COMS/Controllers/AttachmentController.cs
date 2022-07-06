c7b1d59b4e9b639f495433319ee2493eb2d239cb
kamrul <kamrul@asabd.org>
2022-07-06 11:38:20 +0600

    Attachment Service and Controller added

---
+++ COMS/Controllers/AttachmentController.cs
@@ -0,0 +1,126 @@
+﻿using COMS.Security;
+using Core.RequestModels;
+using Core.Service;
+using Core.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using static Core.Common.Enums;
+
+namespace COMS.Controllers
+{
+    [Route("api/[controller")]
+    [Authorize]
+    public class AttachmentController : Controller
+    {
+        private readonly IAttachmentService _attachmentService;
+
+        public AttachmentController(IAttachmentService attachmentService)
+        {
+            _attachmentService = attachmentService;
+        }
+
+        [ClaimRequirement(PermissionType.Admin, PermissionType.Checker, PermissionType.Maker, PermissionType.Viewer)]
+        [HttpPost("Attachment")]
+        public ActionResult SaveAttachment([FromForm] AttachmentRequestModel attachmentRequestModel)
+        {
+            if (attachmentRequestModel.AttachmentTypeId == 0 || attachmentRequestModel.MemberId == 0 || attachmentRequestModel.File.Length == 0)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                return Ok(_attachmentService.SaveAttachment(attachmentRequestModel));
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message, null, (int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [ClaimRequirement(PermissionType.Admin, PermissionType.Checker, PermissionType.Maker, PermissionType.Viewer)]
+        [HttpGet("Attachments")]
+        public List<AttachmentResponse> GetAttachments()
+        {
+            return _attachmentService.GetAttachments();
+        }
+
+        [ClaimRequirement(PermissionType.Admin, PermissionType.Checker, PermissionType.Maker, PermissionType.Viewer)]
+        [HttpGet("Attachment/{memberId}")]
+        public List<AttachmentResponse> GetAttachmentByMemberId(int memberId)
+        {
+            return _attachmentService.GetAttachmentByMemberId(memberId);
+        }
+
+        [ClaimRequirement(PermissionType.Admin, PermissionType.Checker, PermissionType.Maker, PermissionType.Viewer)]
+        [HttpGet("Attachment/{id}")]
+        public AttachmentResponse GetAttachment(int id)
+        {
+            return _attachmentService.GetAttachment(id);
+        }
+
+        [ClaimRequirement(PermissionType.Admin, PermissionType.Checker, PermissionType.Maker, PermissionType.Viewer)]
+        [HttpGet("DownloadAttachment")]
+        public ActionResult DownloadAttachment(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest("Id cannot be zero");
+            }
+
+            try
+            {
+                Stream fileStream = _attachmentService.GetAttachmentFile(id);
+                if(fileStream == null)
+                {
+                    return NotFound();
+                }
+                return File(fileStream, "application/octet-stream");
+            }
+            catch (FileNotFoundException fex)
+            {
+                //_logger.Error(fex.Message);
+                return Problem(fex.Message, null, (int)HttpStatusCode.NotFound);
+            }
+            catch(Exception ex)
+            {
+                //_logger.Error(ex.Message);
+                return Problem(ex.Message, null, (int)HttpStatusCode.InternalServerError);
+            }
+            finally
+            {
+                //_logger.Information("Downloading successfully downloaded");
+            }
+        }
+
+        [ClaimRequirement(PermissionType.Admin)]
+        [HttpDelete("Attachment/{attachmentId}")]
+        public ActionResult DeleteAttachment(int attachmentId)
+        {
+            if(attachmentId == 0)
+            {
+                // _logger.Information("Id cannot be zero");
+                return BadRequest("Id cannot be zero");
+            }
+
+            try
+            {
+                _attachmentService.DeleteAttachment(attachmentId);
+                return Ok();
+            }
+            catch(Exception ex)
+            {   
+                //_logger.Error(ex.Message);
+                return Problem(ex.Message, null, (int)HttpStatusCode.InternalServerError);
+            }
+            finally
+            {
+                //_logger.Information("Attachment successfully deleted");
+            }
+        }
+    }
+}
---
+++ Service/AttachmentService.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Core.FileStore;
+using Core.Repository;
+using Core.RequestModels;
+using Core.Service;
+using Core.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class AttachmentService : IAttachmentService
+    {
+        private readonly IMapper _mapper;
+        private readonly IAttachmentRepository _attachmentRepository;
+        private readonly IConfiguration _configuration;
+        private readonly IFileStore _fileStore;
+
+        public AttachmentService(IMapper mapper, IAttachmentRepository attachmentRepository, IConfiguration configuration, IFileStore fileStore)
+        {
+            _mapper = mapper;
+            _attachmentRepository = attachmentRepository;
+            _configuration = configuration;
+            _fileStore = fileStore;
+        }
+
+        public AttachmentResponse GetAttachment(int attachmentId)
+        {
+            return _mapper.Map<AttachmentResponse>(_attachmentRepository.GetById(attachmentId));
+        }
+
+        public List<AttachmentResponse> GetAttachmentByMemberId(int memberId)
+        {
+            return _mapper.Map<List<AttachmentResponse>>(_attachmentRepository.GetAll().Where(x => x.MemberId == memberId).ToList());
+        }
+
+        public Stream GetAttachmentFile(int attachmentId)
+        {
+            string attachmentPath = _configuration["LocalFileStore:Path"];
+            Attachment attachment = _attachmentRepository.GetById(attachmentId);
+            try
+            {
+                return _fileStore.ReadFile(Path.Combine(attachmentPath, attachment.FileGUID));
+            }
+            catch(Exception ex)
+            {
+                throw new FileNotFoundException(ex.Message);
+            }
+        }
+
+        public List<AttachmentResponse> GetAttachments()
+        {
+            return _mapper.Map<List<AttachmentResponse>>(_attachmentRepository.GetAll());
+        }
+
+        public AttachmentResponse SaveAttachment(AttachmentRequestModel attachmentRequestModel)
+        {
+            string attachmentPath = _configuration["LocalFileStore:Path"];
+            string fileGUID = Guid.NewGuid().ToString();
+            IFileInfo fileInfo = new LocalFileInfo
+            {
+                Name = attachmentRequestModel.FileName,
+                FullName = Path.Combine(attachmentPath, fileGUID)
+            };
+            _fileStore.WriteFile(fileInfo, attachmentRequestModel.File.OpenReadStream());
+
+            Attachment attachment = _mapper.Map<Attachment>(attachmentRequestModel);
+            attachment.FileGUID = fileGUID;
+
+            return _mapper.Map<AttachmentResponse>(_attachmentRepository.Add(attachment));
+        }
+        public void DeleteAttachment(int attachmentId)
+        {
+            string attachmentPath = _configuration["LoaclFileStore:Path"];
+            Attachment attachment = _attachmentRepository.GetById(attachmentId);
+            _attachmentRepository.Delete(attachment);
+            IFileInfo fileInfo = new LocalFileInfo
+            {
+                Name = attachment.FileName,
+                FullName = Path.Combine(attachmentPath, attachment.FileGUID)
+            };
+            _fileStore.DeleteFile(fileInfo);
+        }
+    }
+}
