7dfaf6be49f05934eca43496318c10d71a25c4bc
kamrul <kamrul@asabd.org>
2022-07-06 11:42:38 +0600

    BaseApiController Added

---
+++ COMS/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+
+namespace COMS.Controllers
+{
+    public class BaseApiController : ControllerBase
+    {
+        protected string GetLoggedInUserEmail()
+        {
+            return this.User.FindFirst(ClaimTypes.Email)?.Value;
+        }
+
+        protected int GetLoggedInId()
+        {
+            return Convert.ToInt32(this.User.FindFirst(ClaimTypes.Name).Value);
+        }
+    }
+}
