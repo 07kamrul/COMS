35cc1f0bcf471eaa3e0eed129403c336d2613604
kamrul <kamrul@asabd.org>
2022-07-16 11:45:29 +0600

    Programs and appsettings added

--- COMS/Program.cs
+++ COMS/Program.cs
@@ -1,11 +1,10 @@
+using System;
+using System.Diagnostics;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
 
 namespace COMS
 {
@@ -13,14 +12,52 @@ namespace COMS
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                using IHost host = CreateHostBuilder(args).Build();
+                host.Run();
+            }
+            catch(Exception ex)
+            {
+                if(Log.Logger == null || Log.Logger.GetType().Name == "SilentLogger")
+                {
+                    Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .WriteTo.Console()
+                        .WriteTo.File(Path.Combine(Environment.CurrentDirectory, "logs", "app_log.log"), rollingInterval: RollingInterval.Day)
+                        .CreateLogger();
+                }
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .CaptureStartupErrors(true)
+                    .ConfigureAppConfiguration(config =>
+                    {
+                        config.AddJsonFile("appsettings.Development.json", optional: true);
+                    })
+                    .UseSerilog((hostingContext, loggerConfiguration) =>
+                    {
+                        loggerConfiguration
+                        .ReadFrom.Configuration(hostingContext.Configuration)
+                        .Enrich.FromLogContext()
+                        .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
+                        .Enrich.WithProperty("Environment", hostingContext.HostingEnvironment);
+#if DEBUG
+                        loggerConfiguration.Enrich.WithProperty("DebuggerAttached", Debugger.IsAttached);
+#endif
+                    });
                 });
     }
 }
