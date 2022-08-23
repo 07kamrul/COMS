26cc4f234f7959fb2544d78f7ef6da3a4c104152
kamrul <kamrul@asabd.org>
2022-08-23 14:51:08 +0600

    Projects and Transaction table added

---
+++ Databases/Projects.sql
@@ -0,0 +1,26 @@
+CREATE TABLE [dbo].[Projects](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ProjectName] [nvarchar](100) NOT NULL,
+	[ProjectSize] [nvarchar](100) NOT NULL,
+	[Location] [nvarchar](100) NOT NULL,
+	[MemberId] [int] NOT NULL,
+	[TotalCost] [int] NOT NULL,
+	[StartDate] [date] NOT NULL,
+	[EndDate] [date] NOT NULL,
+	[CreatedBy] [int] NOT NULL,
+	[CreationDate] [date] NOT NULL,
+	[ModifiedBy] [int] NULL,
+	[ModificationDate] [date] NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_Projects] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Projects] ADD  CONSTRAINT [DF_Projects_CreatedDate]  DEFAULT (getdate()) FOR [CreationDate]
+GO
+
+ALTER TABLE [dbo].[Projects] ADD  CONSTRAINT [DF_Projects_IsActive]  DEFAULT ((1)) FOR [IsActive]
+GO
--- Databases/Transaction.sql
+++ Databases/Transaction.sql
@@ -1,7 +1,7 @@
-CREATE TABLE [dbo].[Transaction](
+CREATE TABLE [dbo].[Transactions](
     [Id] [int] IDENTITY(1,1) NOT NULL,
     [MemberId] [int] NOT NULL,
-    [TransactionAmounts] numeric(18,2) NOT NULL,
+    [TransactionAmounts] [numeric](18,2) NOT NULL,
     [TransactionType] [int] NOT NULL,
     [TransactionDate] [date] NOT NULL,
     [CreatedBy] [int] NOT NULL,
@@ -9,15 +9,15 @@ CREATE TABLE [dbo].[Transaction](
     [ModifiedBy] [int] NULL,
     [ModificationDate] [date] NULL,
     [IsActive] [bit] NOT NULL,
- CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_Transactions] PRIMARY KEY CLUSTERED 
 (
     [Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[Transaction] ADD  CONSTRAINT [DF_Transaction_CreatedDate]  DEFAULT (getdate()) FOR [CreationDate]
+ALTER TABLE [dbo].[Transactions] ADD  CONSTRAINT [DF_Transactions_CreatedDate]  DEFAULT (getdate()) FOR [CreationDate]
 GO
 
-ALTER TABLE [dbo].[Transaction] ADD  CONSTRAINT [DF_Transaction_IsActive]  DEFAULT ((1)) FOR [IsActive]
+ALTER TABLE [dbo].[Transactions] ADD  CONSTRAINT [DF_Transactions_IsActive]  DEFAULT ((1)) FOR [IsActive]
 GO
