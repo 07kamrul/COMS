e77df4dc4bbe14fa2c3b75bf01dc37269432cbda
kamrul <kamrul@asabd.org>
2022-09-21 15:58:17 +0600

    Account model updated

--- Model/Account.cs
+++ Model/Account.cs
@@ -17,11 +17,11 @@ namespace Model
         public int ProjectId { get; set; }
         public DateTime OpaningDate { get; set; }
         public DateTime? ClosingDate { get; set; }
-        public double? DueAmounts { get; set; }
-        public double PayableAmounts { get; set; }
-        public double? TotalAmounts { get; set; }
+        public decimal? DueAmounts { get; set; }
+        public decimal PayableAmounts { get; set; }
+        public decimal? TotalAmounts { get; set; }
         public bool IsVerified { get; set; }
-        public string? VerifiedBy { get; set; }
+        public int? VerifiedBy { get; set; }
         public DateTime? VerificationDate { get; set; }
     }
 }
